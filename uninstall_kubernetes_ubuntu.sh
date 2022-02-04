2c54cadd116ea4ebe0032c0d0f81869cb872b742
davitmk <davitmk@hotmail.com>
2022-02-04 20:43:41 +0400

    Updated uninstall_kubernetes_ubuntu.sh

--- uninstall_kubernetes_ubuntu.sh
+++ uninstall_kubernetes_ubuntu.sh
@@ -3,4 +3,15 @@ if [ "$(id -u)" -ne 0 ]; then
         exit 1
 fi
 
+printf 'y\n' | kubeadm reset
+
+apt-get purge -y docker-ce containerd.io
+apt-get purge -y kubeadm kubectl kubelet kubernetes-cni kube*
+sudo apt-get autoremove -y
+
+rm -r /etc/cni/net.d
+rm -r /etc/kubernetes
+rm -rf ~/.kube
+
+iptables -F
 ufw enable
