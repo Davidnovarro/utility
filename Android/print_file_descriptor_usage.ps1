16b347f1c8ceb0515dad62fe06d2e244360e56c6
davitmk <davitmk@hotmail.com>
2023-07-22 15:06:44 +0400

    Minor Changes

--- Android/print_file_descriptor_usage.ps1
+++ Android/print_file_descriptor_usage.ps1
@@ -5,7 +5,12 @@ while ($true) {
 
     if($processId)
     {
-        Write-Output $(adb shell run-as $APP_PID "ls -l /proc/${processId}/fd | wc -l")
+        $usage = $(adb shell run-as $APP_PID "ls -l /proc/${processId}/fd | wc -l")
+        Clear-Host
+        Write-Output "File Descriptors: $usage"
+    }else {
+        Clear-Host
+        Write-Output "App is not running ($APP_PID)"
     }
     
     Start-Sleep 1
