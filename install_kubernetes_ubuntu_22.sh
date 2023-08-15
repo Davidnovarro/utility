1486967bfe2d27da192ec8ed67472dc0208d7d79
davitmk <davitmk@hotmail.com>
2023-08-15 11:00:23 +0400

    Improved: Install Kubernetes for Ubuntu 22 script
    Added: Persistent Variable functions
    Added: We split the installation into phases, so we will be able to restart the machine

--- install_kubernetes_ubuntu_22.sh
+++ install_kubernetes_ubuntu_22.sh
@@ -14,12 +14,12 @@
 export DEBIAN_FRONTEND="noninteractive"
 BASE_PATH=$(readlink -f "$0" | xargs dirname)
 #Make sure versions are compatible with each other
-INSTALL_KUBE_VERSION='1.26.1-00'
-INSTALL_CALICO_VERSION='3.25.0'
-CONTAINERD_VERSION="1.6.16"
-RUNC_VERSION="1.1.1"
-CNI_PLUGINS_VERSION="1.2.0"
-POD_NETWORK_CIDR='10.10.0.0/16'
+INSTALL_KUBE_VERSION='1.27.4-00'
+INSTALL_CALICO_VERSION='3.26.1'
+CONTAINERD_VERSION="1.7.3"
+RUNC_VERSION="1.1.9"
+CNI_PLUGINS_VERSION="1.3.0"
+POD_NETWORK_CIDR='192.168.0.0/16'
 
 #region FUNCTIONS
 ReadYesNo()
@@ -144,126 +144,170 @@ RequireFolder()
     fi
 }
 
-#endregion
+# Sets the persistent variable
+# arg1 is KEY, arg2 is VALUE
+SetVariable()
+{
+    echo $2 | tee "$BASE_PATH/$1.txt" > /dev/null 2>/dev/null
+}
 
+# Gets the persistent variable if does not exist sets the default value
+# arg1 is KEY, arg2 is the default value
+# Usage example
+# if [ $(GetVariable "my_key" 0) = 99 ]; then
+#     echo "Value is 99"
+# fi
+GetVariable()
+{ 
+    if ! test -f "$BASE_PATH/$1.txt"; then
+        echo $2
+    else
+        cat "$BASE_PATH/$1.txt"
+    fi
+}
+
+# Sets the persistent variable
+# arg1 is KEY
+RemoveVariable()
+{
+    rm "$BASE_PATH/$1.txt"
+}
+
+#endregion
 
 if [ "$(id -u)" -ne 0 ]; then
         echo 'This script must be run by root' >&2
         exit 1
 fi
 
-ReadYesNo "Is this a Master Node?"
-IS_MASTER_NODE=$YES
-
-ReadTextInput "Please input a unique name for a node"
-HOST_NAME=$TEXT_INPUT
-OLD_HOST_NAME=$(hostname -s)
-hostnamectl set-hostname $HOST_NAME
-
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
-
-EXTERNAL_IP=$(curl checkip.amazonaws.com)
-
-# Add required lines to /etc/hosts if they do not exts already
-if ! grep -q "localhost" "/etc/hosts"; then
-  echo "127.0.0.1 localhost" >> "/etc/hosts"
-  echo "::1 localhost" >> "/etc/hosts"
-fi
-
-if ! grep -q "ip6-localhost" "/etc/hosts"; then
-  echo "# The following lines are desirable for IPv6 capable hosts" >> "/etc/hosts"
-  echo "::1     ip6-localhost ip6-loopback" >> "/etc/hosts"
-  echo "fe00::0 ip6-localnet" >> "/etc/hosts"
-  echo "ff00::0 ip6-mcastprefix" >> "/etc/hosts"
-  echo "ff02::1 ip6-allnodes" >> "/etc/hosts"
-  echo "ff02::2 ip6-allrouters" >> "/etc/hosts"
-fi
-
-if ! grep -q "${EXTERNAL_IP}" "/etc/hosts" ; then
-  echo "${EXTERNAL_IP} ${HOST_NAME}" >> "/etc/hosts"
-fi
-
-#Prepeare to install container runtime : https://kubernetes.io/docs/setup/production-environment/container-runtimes/
-printf "overlay\nbr_netfilter\n" | tee /etc/modules-load.d/containerd.conf
-
-modprobe overlay
-modprobe br_netfilter
-
-printf "net.bridge.bridge-nf-call-iptables = 1\nnet.ipv4.ip_forward = 1\nnet.bridge.bridge-nf-call-ip6tables = 1\n" | tee /etc/sysctl.d/99-kubernetes-cri.conf
-
-sysctl --system
-
-wget https://github.com/containerd/containerd/releases/download/v$CONTAINERD_VERSION/containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz -P /tmp/
-tar Cxzvf /usr/local /tmp/containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz
-wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service -P /etc/systemd/system/
-systemctl daemon-reload
-systemctl enable --now containerd
-
-
-wget https://github.com/opencontainers/runc/releases/download/v$RUNC_VERSION/runc.amd64 -P /tmp/
-install -m 755 /tmp/runc.amd64 /usr/local/sbin/runc
-
-wget https://github.com/containernetworking/plugins/releases/download/v$CNI_PLUGINS_VERSION/cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz -P /tmp/
-mkdir -p /opt/cni/bin
-tar Cxzvf /opt/cni/bin /tmp/cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz
-
-mkdir -p /etc/containerd
-#Changing the SystemdCgroup to true
-containerd config default | sed -e 's|SystemdCgroup = false|SystemdCgroup = true|' | tee /etc/containerd/config.toml
-if ! grep -q "SystemdCgroup = true" "/etc/containerd/config.toml"; then
-  echo "Error: unable to set SystemdCgroup = true in /etc/containerd/config.toml file"
-  exit 1
-fi
-systemctl restart containerd
-
-#Disable Firewall
-ufw disable
-
-#Disable swap
-swapoff -a; sed -i '/swap/d' /etc/fstab
-
-sleep 15
-
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
-sudo curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | gpg --dearmor --batch --yes -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet=$INSTALL_KUBE_VERSION kubeadm=$INSTALL_KUBE_VERSION kubectl=$INSTALL_KUBE_VERSION
-sudo apt-mark hold kubelet kubeadm kubectl
-
-
-if $IS_MASTER_NODE; then
-  #Initialize Kubernetes Cluster 
-  kubeadm init --apiserver-advertise-address=$EXTERNAL_IP --pod-network-cidr=$POD_NETWORK_CIDR --ignore-preflight-errors=all --apiserver-cert-extra-sans=$EXTERNAL_IP --skip-token-print --skip-certificate-key-print
-  
-  export KUBECONFIG='/etc/kubernetes/admin.conf'
-  #Remove the taints on the master so that you can schedule pods on it.
-  kubectl taint nodes --all node-role.kubernetes.io/control-plane- > /dev/null 2>/dev/null
-  kubectl taint nodes --all node-role.kubernetes.io/master- > /dev/null 2>/dev/null
- 
-
-  #Deploy Calico network
-  kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v$INSTALL_CALICO_VERSION/manifests/tigera-operator.yaml
-  curl https://raw.githubusercontent.com/projectcalico/calico/v$INSTALL_CALICO_VERSION/manifests/custom-resources.yaml | sed -e "s|192.168.0.0/16|${POD_NETWORK_CIDR}|" | kubectl create -f -
-
-  #Copy the config file to main folder
-  mkdir -p $HOME/.kube
-  cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-  chown $(id -u):$(id -g) $HOME/.kube/config
-
-  #Print the cluster join command
-  kubeadm token create --print-join-command
-else
-    echo "You need to join the master node, run this command on master to get the token: kubeadm token create --print-join-command"
-    # Follow the join instructions manually and then run the commands commented below
-    # kubectl taint nodes --all node-role.kubernetes.io/control-plane- > /dev/null 2>/dev/null
-    # kubectl taint nodes --all node-role.kubernetes.io/master- > /dev/null 2>/dev/null
-fi
-
-#Remove the tmp folder with all temporary files
-rm -rf /tmp/
-
 echo "alias k='kubectl'" | tee ~/.bash_aliases
-echo 'Run this command to activate aliases: source ~/.bash_aliases' 
+
+if [ $(GetVariable "install_kubernetes_phase" 0) = "FINISHED" ]; then
+    echo "Install phase is FINISHED"
+    exit 0
+fi
+
+if [ $(GetVariable "install_kubernetes_phase" 0) = 0 ]; then
+    echo "Starting install phase 0"
+    ReadYesNo "Is this a Master Node?"
+    IS_MASTER_NODE=$YES
+
+    ReadTextInput "Please input a unique name for a node"
+    HOST_NAME=$TEXT_INPUT
+    OLD_HOST_NAME=$(hostname -s)
+    hostnamectl set-hostname $HOST_NAME
+
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https ca-certificates curl
+
+    EXTERNAL_IP=$(curl checkip.amazonaws.com)
+
+    # Add required lines to /etc/hosts if they do not exts already
+    if ! grep -q "localhost" "/etc/hosts"; then
+        echo "127.0.0.1 localhost" >> "/etc/hosts"
+        echo "::1 localhost" >> "/etc/hosts"
+    fi
+
+    if ! grep -q "ip6-localhost" "/etc/hosts"; then
+        echo "# The following lines are desirable for IPv6 capable hosts" >> "/etc/hosts"
+        echo "::1     ip6-localhost ip6-loopback" >> "/etc/hosts"
+        echo "fe00::0 ip6-localnet" >> "/etc/hosts"
+        echo "ff00::0 ip6-mcastprefix" >> "/etc/hosts"
+        echo "ff02::1 ip6-allnodes" >> "/etc/hosts"
+        echo "ff02::2 ip6-allrouters" >> "/etc/hosts"
+    fi
+
+    if ! grep -q "${EXTERNAL_IP}" "/etc/hosts" ; then
+        echo "${EXTERNAL_IP} ${HOST_NAME}" >> "/etc/hosts"
+    fi
+
+    #Prepeare to install container runtime : https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+    printf "overlay\nbr_netfilter\n" | tee /etc/modules-load.d/containerd.conf
+
+    modprobe overlay
+    modprobe br_netfilter
+
+    printf "net.bridge.bridge-nf-call-iptables = 1\nnet.ipv4.ip_forward = 1\nnet.bridge.bridge-nf-call-ip6tables = 1\n" | tee /etc/sysctl.d/99-kubernetes-cri.conf
+
+    sysctl --system
+
+    wget https://github.com/containerd/containerd/releases/download/v$CONTAINERD_VERSION/containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz -P /tmp/
+    tar Cxzvf /usr/local /tmp/containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz
+    wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service -P /etc/systemd/system/
+    systemctl daemon-reload
+    systemctl enable --now containerd
+
+
+    wget https://github.com/opencontainers/runc/releases/download/v$RUNC_VERSION/runc.amd64 -P /tmp/
+    install -m 755 /tmp/runc.amd64 /usr/local/sbin/runc
+
+    wget https://github.com/containernetworking/plugins/releases/download/v$CNI_PLUGINS_VERSION/cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz -P /tmp/
+    mkdir -p /opt/cni/bin
+    tar Cxzvf /opt/cni/bin /tmp/cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz
+
+    mkdir -p /etc/containerd
+    #Changing the SystemdCgroup to true
+    containerd config default | sed -e 's|SystemdCgroup = false|SystemdCgroup = true|' | tee /etc/containerd/config.toml
+    if ! grep -q "SystemdCgroup = true" "/etc/containerd/config.toml"; then
+        echo "Error: unable to set SystemdCgroup = true in /etc/containerd/config.toml file"
+        exit 1
+    fi
+    systemctl restart containerd
+
+    #Disable Firewall
+    ufw disable
+
+    #Disable swap
+    swapoff -a; sed -i '/swap/d' /etc/fstab
+    
+    #Remove the tmp folder with all temporary files
+    rm -rf /tmp/
+
+    SetVariable "install_kubernetes_phase" 1
+    echo "Install phase 0 is finished, please reboot and run this script again"
+    exit 0
+fi
+
+if [ $(GetVariable "install_kubernetes_phase" 0) = 1 ]; then
+    echo "Starting install phase 1"
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+    sudo curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | gpg --dearmor --batch --yes -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+    sudo apt-get update
+    sudo apt-get install -y kubelet=$INSTALL_KUBE_VERSION kubeadm=$INSTALL_KUBE_VERSION kubectl=$INSTALL_KUBE_VERSION
+    sudo apt-mark hold kubelet kubeadm kubectl
+
+    if $IS_MASTER_NODE; then
+        #Initialize Kubernetes Cluster 
+        kubeadm init --apiserver-advertise-address=$EXTERNAL_IP --pod-network-cidr=$POD_NETWORK_CIDR --ignore-preflight-errors=all --apiserver-cert-extra-sans=$EXTERNAL_IP --skip-token-print --skip-certificate-key-print
+        
+        export KUBECONFIG='/etc/kubernetes/admin.conf'
+        
+        #Deploy Calico network
+        kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v$INSTALL_CALICO_VERSION/manifests/tigera-operator.yaml
+        curl https://raw.githubusercontent.com/projectcalico/calico/v$INSTALL_CALICO_VERSION/manifests/custom-resources.yaml | sed -e "s|192.168.0.0/16|${POD_NETWORK_CIDR}|" | kubectl create -f -
+
+        #Remove the taints on the master so that you can schedule pods on it.
+        kubectl taint nodes --all node-role.kubernetes.io/control-plane- > /dev/null 2>/dev/null
+        kubectl taint nodes --all node-role.kubernetes.io/master- > /dev/null 2>/dev/null
+
+        #Copy the config file to main folder
+        mkdir -p $HOME/.kube
+        cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+        chown $(id -u):$(id -g) $HOME/.kube/config
+
+        #Print the cluster join command
+        kubeadm token create --print-join-command
+    else
+        echo "You need to join the master node, run this command on master to get the token: kubeadm token create --print-join-command"
+        # Follow the join instructions manually and then run the commands commented below
+        # kubectl taint nodes --all node-role.kubernetes.io/control-plane- > /dev/null 2>/dev/null
+        # kubectl taint nodes --all node-role.kubernetes.io/master- > /dev/null 2>/dev/null
+    fi
+    echo "Install is finished"
+    SetVariable "install_kubernetes_phase" "FINISHED"
+    exit 0
+fi
+
+#echo 'Run this command to activate aliases: source ~/.bash_aliases'
