da50a72fda8d069c01551c2328d56d07810cc2d9
davitmk <davitmk@hotmail.com>
2023-08-21 17:24:00 +0400

    Added: Support for IPVS k8s install script

--- install_kubernetes_ubuntu_22.sh
+++ install_kubernetes_ubuntu_22.sh
@@ -20,9 +20,6 @@ CONTAINERD_VERSION="1.7.3"
 RUNC_VERSION="1.1.9"
 CNI_PLUGINS_VERSION="1.3.0"
 POD_NETWORK_CIDR='192.168.0.0/16'
-sudo apt-get -qq update
-sudo apt-get -qq install curl -y
-EXTERNAL_IP=$(curl -s checkip.amazonaws.com)
 
 #region FUNCTIONS
 ReadYesNo()
@@ -178,6 +175,29 @@ RemoveVariable()
 
 #endregion
 
+ReadYesNo "Is this a Master Node?"
+IS_MASTER_NODE=$YES
+
+if $IS_MASTER_NODE; then
+    echo 'Please select IPVS scheduling option: https://linux.die.net/man/8/ipvsadm'
+    echo '  disabled, rr: are good options for Game Server cluster'
+    echo '  sh: Source Hashing is required for Party cluster'
+
+    # ALL_IPVS_OPTIONS="disabled sh rr wrr lc wlc lblc lblcr dh sed nq"
+    PS3='Please select IPVS scheduling option: '
+    ALL_IPVS_OPTIONS="disabled rr sh"
+    select IPVS_SCHEDULER in $ALL_IPVS_OPTIONS
+    do
+        if [[ " $ALL_IPVS_OPTIONS " = *" $IPVS_SCHEDULER "* ]]; then
+            break
+        fi
+    done
+fi
+
+sudo apt-get -qq update
+sudo apt-get -qq install curl -y
+EXTERNAL_IP=$(curl -s checkip.amazonaws.com)
+
 if [ "$(id -u)" -ne 0 ]; then
         echo 'This script must be run by root' >&2
         exit 1
@@ -191,10 +211,9 @@ if [ $(GetVariable "install_kubernetes_phase" 0) = "FINISHED" ]; then
     exit 0
 fi
 
+#region Install Phase 0
 if [ $(GetVariable "install_kubernetes_phase" 0) = 0 ]; then
     echo "Starting install phase 0"
-    ReadYesNo "Is this a Master Node?"
-    IS_MASTER_NODE=$YES
 
     ReadTextInput "Please input a unique name for a node"
     HOST_NAME=$TEXT_INPUT
@@ -223,7 +242,20 @@ if [ $(GetVariable "install_kubernetes_phase" 0) = 0 ]; then
         echo "${EXTERNAL_IP} ${HOST_NAME}" >> "/etc/hosts"
     fi
 
-    #Prepeare to install container runtime : https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+    # Install IPVS
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get install -y ipset ipvsadm
+    #Adding kernel modules required by IPVS to load on boot time
+    printf "ip_vs\nip_vs_rr\nip_vs_wrr\nip_vs_sh\nnf_conntrack\n" | tee /etc/modules-load.d/ipvs.conf > /dev/null 2>/dev/null
+    modprobe ip_vs
+    modprobe ip_vs_rr
+    modprobe ip_vs_wrr
+    modprobe ip_vs_sh
+    modprobe nf_conntrack
+
+    # Install Containerd : https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+    #Adding kernel modules required by Containerd to load on boot time
     printf "overlay\nbr_netfilter\n" | tee /etc/modules-load.d/containerd.conf > /dev/null 2>/dev/null
 
     modprobe overlay
@@ -269,45 +301,77 @@ if [ $(GetVariable "install_kubernetes_phase" 0) = 0 ]; then
     echo "Install phase 0 is finished, please reboot and run this script again"
     exit 0
 fi
+#endregion
 
+#region Install Phase 1
 if [ $(GetVariable "install_kubernetes_phase" 0) = 1 ]; then
-    echo "Starting install phase 1"
-    sudo apt-get -qq update
-    sudo apt-get install -y apt-transport-https ca-certificates curl gpg
-    sudo curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | gpg --dearmor --batch --yes -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list > /dev/null 2>/dev/null
-    sudo apt-get -qq update
-    sudo apt-get install -y kubelet=$INSTALL_KUBE_VERSION kubeadm=$INSTALL_KUBE_VERSION kubectl=$INSTALL_KUBE_VERSION
-    sudo apt-mark hold kubelet kubeadm kubectl
+echo "Starting install phase 1"
+sudo apt-get -qq update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | gpg --dearmor --batch --yes -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list > /dev/null 2>/dev/null
+sudo apt-get -qq update
+sudo apt-get install -y kubelet=$INSTALL_KUBE_VERSION kubeadm=$INSTALL_KUBE_VERSION kubectl=$INSTALL_KUBE_VERSION
+sudo apt-mark hold kubelet kubeadm kubectl
 
-    if $IS_MASTER_NODE; then
-        #Initialize Kubernetes Cluster 
-        kubeadm init --apiserver-advertise-address=$EXTERNAL_IP --pod-network-cidr=$POD_NETWORK_CIDR --ignore-preflight-errors=all --apiserver-cert-extra-sans=$EXTERNAL_IP --skip-token-print --skip-certificate-key-print
-        
-        export KUBECONFIG='/etc/kubernetes/admin.conf'
-        
-        #Deploy Calico network
-        kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v$INSTALL_CALICO_VERSION/manifests/tigera-operator.yaml
-        curl https://raw.githubusercontent.com/projectcalico/calico/v$INSTALL_CALICO_VERSION/manifests/custom-resources.yaml | sed -e "s|192.168.0.0/16|${POD_NETWORK_CIDR}|" | kubectl create -f -
+if $IS_MASTER_NODE; then
 
-        #Remove the taints on the master so that you can schedule pods on it.
-        kubectl taint nodes --all node-role.kubernetes.io/control-plane- > /dev/null 2>/dev/null
-        kubectl taint nodes --all node-role.kubernetes.io/master- > /dev/null 2>/dev/null
+cat > kubeadm.yaml <<EOF
+apiVersion: kubeadm.k8s.io/v1beta3
+kind: InitConfiguration
+localAPIEndpoint:
+  advertiseAddress: $EXTERNAL_IP
+---
+apiVersion: kubeadm.k8s.io/v1beta3
+kind: ClusterConfiguration
+networking:
+  podSubnet: $POD_NETWORK_CIDR
+EOF
 
-        #Copy the config file to main folder
-        mkdir -p $HOME/.kube
-        cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-        chown $(id -u):$(id -g) $HOME/.kube/config
-
-        #Print the cluster join command
-        kubeadm token create --print-join-command
-    else
-        echo "You need to join the master node, run this command on master to get the token: kubeadm token create --print-join-command"
-        # Follow the join instructions manually and then run the commands commented below
-        # kubectl taint nodes --all node-role.kubernetes.io/control-plane- > /dev/null 2>/dev/null
-        # kubectl taint nodes --all node-role.kubernetes.io/master- > /dev/null 2>/dev/null
-    fi
-    echo "Install is finished"
-    SetVariable "install_kubernetes_phase" "FINISHED"
-    exit 0
+if [ $IPVS_SCHEDULER != "disabled" ]; then
+    cat >> kubeadm.yaml <<EOF
+---
+apiVersion: kubeproxy.config.k8s.io/v1alpha1
+kind: KubeProxyConfiguration
+mode: ipvs
+ipvs:
+  scheduler: $IPVS_SCHEDULER
+EOF
 fi
+    #Initialize Kubernetes Cluster 
+    kubeadm init --config kubeadm.yaml --ignore-preflight-errors=all --skip-token-print --skip-certificate-key-print
+    
+    export KUBECONFIG='/etc/kubernetes/admin.conf'
+
+    # Deprecated, we assign scheduler option in kubeadm.yaml file, but this lines could be helpfull to modify the scheduler post install though     
+    # if [ $IPVS_SCHEDULER != "disabled" ]; then
+    #    kubectl get configmap kube-proxy -n kube-system -o yaml | sed -e "s|scheduler: \"\"|scheduler: ${IPVS_SCHEDULER}|" | kubectl apply -f -
+    #    kubectl delete pods -l k8s-app=kube-proxy -n kube-system
+    # fi
+
+    #Deploy Calico network
+    kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v$INSTALL_CALICO_VERSION/manifests/tigera-operator.yaml
+    curl -s https://raw.githubusercontent.com/projectcalico/calico/v$INSTALL_CALICO_VERSION/manifests/custom-resources.yaml | sed -e "s|192.168.0.0/16|${POD_NETWORK_CIDR}|" | kubectl create -f -
+
+    #Remove the taints on the master so that you can schedule pods on it.
+    kubectl taint nodes --all node-role.kubernetes.io/control-plane- > /dev/null 2>/dev/null
+    kubectl taint nodes --all node-role.kubernetes.io/master- > /dev/null 2>/dev/null
+
+    #Copy the config file to main folder
+    mkdir -p $HOME/.kube
+    cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    chown $(id -u):$(id -g) $HOME/.kube/config
+
+    #Print the cluster join command
+    kubeadm token create --print-join-command
+else
+    echo "You need to join the master node, run this command on master to get the token: kubeadm token create --print-join-command"
+    # Follow the join instructions manually and then run the commands commented below
+    # kubectl taint nodes --all node-role.kubernetes.io/control-plane- > /dev/null 2>/dev/null
+    # kubectl taint nodes --all node-role.kubernetes.io/master- > /dev/null 2>/dev/null
+fi
+echo "Install is finished"
+SetVariable "install_kubernetes_phase" "FINISHED"
+exit 0
+fi
+#endregion
